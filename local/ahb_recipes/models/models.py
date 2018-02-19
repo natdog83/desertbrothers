2d4ede1221b12780f5552b4ad60d7805174d7fb7
Nathan <nathan@nathanmccusker.com>
2018-02-18 19:48:48 -0800

    Updating selections3

--- local/ahb_recipes/models/models.py
+++ local/ahb_recipes/models/models.py
@@ -14,10 +14,44 @@ class styles(models.Model):
     style_guide = fields.Char('Style Guide', required=True)
     number = fields.Integer()
     style_letter = fields.Selection((
-      ('A','A'),
-      ('B','B'),
-      ('C','C')),
-      'Style Letter')
+      'A',
+      'B',
+      'C',
+      'D',
+      'E',
+      'F',
+      'G',
+      'H',
+      'I',
+      'J',
+      'K',
+      'L',
+      'M',
+      'N',
+      'O',
+      'P',
+      'Q',
+      'R',
+      'S',
+      'T',
+      'U',
+      'V',
+      'W',
+      'X',
+      'Y',
+      'Z',
+      '0',
+      '1',
+      '2',
+      '3',
+      '4',
+      '5',
+      '6',
+      '7',
+      '8',
+      '9'
+    )'Style Letters')
+    beertype = fields.Selection(('Ale','Lager','Mixed','Mead','Cider','Wheat')'Beer Type')
     og_min = fields.Float()
     og_max = fields.Float()
     fg_min = fields.Float()

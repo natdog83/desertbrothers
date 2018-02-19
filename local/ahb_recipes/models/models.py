a5fc0a3eed4e45f336fa41c490dbc198f28c08ba
Nathan <nathan@nathanmccusker.com>
2018-02-18 19:22:20 -0800

    removing description from styles class

--- local/ahb_recipes/models/models.py
+++ local/ahb_recipes/models/models.py
@@ -9,7 +9,6 @@ class ahb_recipes(models.Model):
 
 class styles(models.Model):
     _name = 'ahb_recipes.styles'
-    _description = 'Styles'
     _rec_name = 'name'
     name = fields.Char('Name', required=True)
     category = fields.Char('Category', required=True)

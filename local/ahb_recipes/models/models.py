d5b033437f691979324acfc70c9dc1c9e3c01b93
Nathan <nathan@nathanmccusker.com>
2018-02-21 11:27:18 -0800

    trying a new dict function

--- local/ahb_recipes/models/models.py
+++ local/ahb_recipes/models/models.py
@@ -82,7 +82,8 @@ class styles(models.Model):
     @api.depends('number')
     def _number_generator(self):
         for record in self:
-            record.number_updated = "%s%s" % (record.number or '',record.style_letter or '')
+            value = dict(VALUES)[record.style_letter]
+            record.number_updated = "%s%s" % (record.number or '',value or '')
 
     @api.multi
     def _calculate_og_range(self):

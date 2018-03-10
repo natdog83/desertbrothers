41363cf550462b372a9a36775dcd389c15010398
Nathan <nathan@nathanmccusker.com>
2018-03-10 01:12:15 -0800

    asdf

--- local/pos_member/static/src/js/pos_2_so.js
+++ local/pos_member/static/src/js/pos_2_so.js
@@ -5,6 +5,7 @@ odoo.define('point_of_sale.pos_2_so', function(require) {
     var devices = require('point_of_sale.devices');
     var gui = require('point_of_sale.gui');
     var screens = require('point_of_sale.screens');
+    var chrome = require('point_of_sale.chrome');
 
     var Pos2SoWidget = PosBaseWidget.extend({
         template : 'Pos2SoWidget',
@@ -15,7 +16,7 @@ odoo.define('point_of_sale.pos_2_so', function(require) {
         start : function() {
             var self = this;
             this.$el.find('.pos-2-so-button').click(function() {
-                self.gui.show_screen('orders');
+                self.gui.show_screen('tabs');
             });
         },
     });
@@ -34,30 +35,35 @@ odoo.define('point_of_sale.pos_2_so', function(require) {
 
     // The screen that allows you to select the orders, see and select the table,
     // as well as edit them.
-    // var OrderScreenWidget = screens.ScreenWidget.extend({
-    //     template: 'OrderScreenWidget',
-    //
-    //     init: function(parent, options){
-    //         this._super(parent, options);
-    //         this.partner_cache = new DomCache();
-    //     },
-    //
-    // });
+    var TabScreenWidget = screens.ScreenWidget.extend({
+        template: 'OrderScreenWidget',
+
+        init: function(parent, options){
+            this._super(parent, options);
+            this.partner_cache = new DomCache();
+        },
+
+    });
 
     gui.define_screen({
-        name: 'orders',
-        widget: OrderScreenWidget
+        'name': 'tabs',
+        'widget': TabScreenWidget,
+        'condition': function(){
+            return this.pos.config.iface_tabs;
+        },
     });
 
     // // Add the OrderScreen to the GUI, and set it as the default screen
-    // chrome.Chrome.include({
-    //     build_widgets: function(){
-    //         this._super();
-    //         if (this.pos.config.iface_orders) {
-    //             this.gui.set_startup_screen('orders');
-    //         }
-    //     },
-    // });
-
+    chrome.Chrome.include({
+        build_widgets: function(){
+            this._super();
+            if (this.pos.config.iface_tabs) {
+                this.gui.set_startup_screen('tabs');
+            }
+        },
+    });
+    return {
+        TabScreenWidget : TabScreenWidget,
+    };
 
 });

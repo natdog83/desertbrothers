3c4fdb5e6d6f80e2604bb85706991ea675431733
Nathan <nathan@nathanmccusker.com>
2018-03-09 23:06:41 -0800

    Adding order screen

--- local/pos_member/static/src/js/pos_2_so.js
+++ local/pos_member/static/src/js/pos_2_so.js
@@ -2,6 +2,7 @@ odoo.define('point_of_sale.pos_2_so', function(require) {
     "use strict";
 
     var PosBaseWidget = require('point_of_sale.BaseWidget');
+    var devices = require('point_of_sale.devices');
     var gui = require('point_of_sale.gui');
     var screens = require('point_of_sale.screens');
 
@@ -31,4 +32,37 @@ odoo.define('point_of_sale.pos_2_so', function(require) {
 
         },
     });
+
+    // The screen that allows you to select the orders, see and select the table,
+    // as well as edit them.
+    var FloorScreenWidget = screens.ScreenWidget.extend({
+        template: 'OrderScreenWidget',
+        show_leftpane: false,
+
+        // Ignore products, discounts, and client barcodes
+        barcode_product_action: function(code){},
+        barcode_discount_action: function(code){},
+        barcode_client_action: function(code){},
+
+    });
+
+    gui.define_screen({
+        'name': 'orders',
+        'widget': OrderScreenWidget,
+        'condition': function(){
+            return this.pos.config.iface_orders;
+        },
+    });
+
+    // Add the OrderScreen to the GUI, and set it as the default screen
+    chrome.Chrome.include({
+        build_widgets: function(){
+            this._super();
+            if (this.pos.config.iface_orders) {
+                this.gui.set_startup_screen('orders');
+            }
+        },
+    });
+
+
 });

0ed5bb7c8bf6f583f28f5d5eaf08ec3730a5bbc7
Nathan McCusker <nathan@nathanmccusker.com>
2019-09-17 17:07:34 -0700

    Adding theme_angryhorse

---
+++ local/theme_angryhorse/__openerp__.py
@@ -0,0 +1,13 @@
+{
+    'author': "Nathan McCusker",
+    'website': "https://nathanmccusker.com",
+    'name': 'Angry Horse Brewing Theme',
+    'summary': 'Website theme for Angry Horse',
+    'description': 'Website Theme for Angry Horse',
+    'category': 'Theme',
+    'version': '0.3',
+    'images': ['images/angryhorse-thumbnail.png'],
+    'depends': ['website'],
+    'data': ['views/angryhorse.xml'],
+    'application': False,
+}

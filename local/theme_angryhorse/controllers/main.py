00f0e7d6151ceab9c771529b2437c566f5d044e7
Nathan McCusker <nathan@nathanmccusker.com>
2019-09-20 22:19:20 -0700

    Adding controller

--- local/theme_angryhorse/controllers/main.py
+++ local/theme_angryhorse/controllers/main.py
@@ -7,5 +7,5 @@ class CustomWebsite(Website):  # Inherit in your custom class
     @http.route('/', type='http', auth="public", website=True)
     def index(self):
         res = super(CustomWebsite, self).index()
-        Teachers = http.request.env['academy.teachers']
+        print('TEST')
         return res

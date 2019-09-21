b630410b74ab5369cfc1984aa9d27d0b42ff1d8b
Nathan McCusker <nathan@nathanmccusker.com>
2019-09-21 01:08:43 -0700

    test

--- local/theme_angryhorse/controllers/main.py
+++ local/theme_angryhorse/controllers/main.py
@@ -6,6 +6,7 @@ from openerp.http import request
 class AngryHorseHomepage(http.Controller):
     @http.route('/academy/academy/', auth='public', website=True)
     def index(self, **kw):
+        staff = http.request.env['hr.employee']
         employee_ids = hr_obj.search(request.cr, request.uid, [('website_published', '=', True)], context=request.context)
         values = {
             'employee_ids': hr_obj.browse(request.cr, request.uid, employee_ids,

84df4d96d55d1859b21b839bdf9eff1660240b2d
Nathan McCusker <nathan@nathanmccusker.com>
2019-09-21 01:44:02 -0700

    test

--- local/theme_angryhorse/controllers/main.py
+++ local/theme_angryhorse/controllers/main.py
@@ -7,16 +7,12 @@ class AngryHorseHomepage(http.Controller):
     @http.route('/academy/academy/', auth='public', website=True)
     def index(self, **kw):
         hr_obj = request.registry['hr.employee']
-        if request.registry['res.users'].has_group(request.cr, request.uid, 'base.group_website_publisher'):
-            employee_ids = hr_obj.search(request.cr, request.uid, [], context=request.context)
-        else:
-            employee_ids = hr_obj.search(request.cr, request.uid, [('website_published', '=', True)], context=request.context)
+        employee_ids = hr_obj.search(request.cr, request.uid, [('website_published', '=', True)], context=request.context)
         values = {
             'employee_ids': hr_obj.browse(request.cr, request.uid, employee_ids,
                                           request.context),
         }
-        for value in values:
-            print value.employee_ids
+        print employee_ids
         return request.website.render("theme_angryhorse.index", values)
 
 

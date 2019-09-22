2fa278db6f66bac3a32fd254bca89b3fd47c0319
Nathan McCusker <nathan@nathanmccusker.com>
2019-09-21 22:52:19 -0700

    new controller

--- local/theme_angryhorse/controllers/main.py
+++ local/theme_angryhorse/controllers/main.py
@@ -1,6 +1,23 @@
 # -*- coding: utf-8 -*-
 from openerp import http
 from openerp.http import request
+from addons.website.controllers.main import Website
+
+
+class Home(Website):
+
+    @http.route(['/', '/index', '/home'], type='http', website=True, auth='public')
+    def index(self, **kw):
+        hr_obj = request.registry['hr.employee']
+        employee_ids = hr_obj.search(request.cr, request.uid, [('website_published', '=', True)], context=request.context)
+        values = {
+            'employee_ids': hr_obj.browse(request.cr, request.uid, employee_ids,
+                                            request.context),
+        }
+        return request.website.render("theme_angryhorse.index", values)
+
+    # super(Website, self).index(**kw)
+    # return http.request.render('my_website.home')
 
 
 class AngryHorseHomepage(http.Controller):

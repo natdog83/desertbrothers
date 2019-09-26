dfabd7dff4af43948a9e16d85622bd9e27121f62
Nathan McCusker <nathan@nathanmccusker.com>
2019-09-25 22:49:32 -0700

    test

--- local/theme_angryhorse/controllers/main.py
+++ local/theme_angryhorse/controllers/main.py
@@ -31,7 +31,7 @@ class AngryHorseHomepage(http.Controller):
         return self(values)
 
     @http.route('/api/menu', auth='public', website=True)
-    def index(self, **kw):
+    def untappdmenu(self, **kw):
         r = requests.get('https://business.untappd.com/api/v1/menus/20670?full=true', auth=('nmccusker@angryhorsebrewing.com', '15TG6vxCrAtyLVWWUsrv'))
         print(r.text)
         return self(r)

53f1b11fd6c05b15f6625fc02efbcfcda5fa923f
Nathan McCusker <nathan@nathanmccusker.com>
2019-09-27 18:05:11 -0700

    test

--- local/theme_angryhorse/controllers/main.py
+++ local/theme_angryhorse/controllers/main.py
@@ -20,16 +20,17 @@ class Home(Website):
     # super(Website, self).index(**kw)
     # return http.request.render('my_website.home')
 
-# class AngryHorseHomepage(http.Controller):
-#     @http.route('/academy/academy/', auth='public', website=True)
-#     def index(self, **kw):
-#         hr_obj = request.registry['hr.employee']
-#         employee_ids = hr_obj.search(request.cr, request.uid, [('website_published', '=', True)], context=request.context)
-#         values = {
-#             'employee_ids': hr_obj.browse(request.cr, request.uid, employee_ids,
-#                                           request.context),
-#         }
-#         return self(values)
+class AngryHorseHomepage(http.Controller):
+    @http.route('/academy/academy/', auth='public', website=True)
+    def index(self, **kw):
+        hr_obj = request.registry['hr.employee']
+        employee_ids = hr_obj.search(request.cr, request.uid, [('website_published', '=', True)], context=request.context)
+        values = {
+            'employee_ids': hr_obj.browse(request.cr, request.uid, employee_ids,
+                                          request.context),
+        }
+        return self(values)
+
 class BeerAPI(Website):
 
     @http.route('/api/brand/', type='http', website=True, auth='public')
